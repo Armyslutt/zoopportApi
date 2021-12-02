4d15d0192c47e6f2e3d1e47db127ba728099544c
Armyslutt <73320529+Armyslutt@users.noreply.github.com>
2021-12-02 04:21:29 -0500

    api zoopport

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const sequelize = require('./database/db');
 
 require('dotenv').config();
 
-
 /* 
 require('./database/relacionesM-M'); */
 
 // Setting
 const PORT = process.env.PORT || 3000;
+
+// Manejar el CORS policity
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "OPTIONS,POST,GET,DELETE,PUT");
+    next();
+  }); 
 
 // Para poder rellenar el req.body
 app.use(express.json());
--- routes/api/animal.js
+++ routes/api/animal.js
@@ -1,67 +1,182 @@
-
 const router = require('express').Router();
+const Animal = require('../../database/models/Animal');
+const TipoAnimal = require('../../database/models/TipoAnimal');
+const Enfermedad = require('../../database/models/Enfermedad');
+const Tratamiento = require('../../database/models/Tratamiento');
+const multer = require('multer');
 
-const Animal = require('../../database/models/animal');
-const EstadoAnimal = require('../../database/models/estadoAnimal');
-const Especie = require('../../database/models/Especie');
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads');
+    },
+    filename: function(req, file, cb){
+        cb(null, Date.now() + file.originalname)
+    }
+})
+
+const upload = multer({ storage: storage})
 
 //consultar todos los Animales
 router.get('/', async(req, res) => {
     const animal = await Animal.findAll(
         {
         include:[{
-            model: Especie,
-            attributes: ['nombreEspecie']
+            model: TipoAnimal,
+            attributes: ['nombreTipoAnimal']
         },
         {
-            model: EstadoAnimal,
-            attributes:['nombreEstadoAnimal']
+            model: Enfermedad,
+            attributes:['nombreEnfermedad']
+        },
+        {
+            model: Tratamiento,
+            attributes:['nombreTratamiento']
         }],
-        attributes: ['nombreAnimal','edad','fechaLlegada','historialTratamiento']
-    });
+    })
+    res.json(animal);
+});
 
+
+//consultar todos los animales adoptados
+router.get('/adoptados', async(req, res) => {
+    const animal = await Animal.findAll(
+        {
+        where: {estadoAnimal: 'adoptado'},
+        include:[{
+            model: TipoAnimal,
+            attributes: ['nombreTipoAnimal']
+        },
+        {
+            model: Enfermedad,
+            attributes:['nombreEnfermedad']
+        },
+        {
+            model: Tratamiento,
+            attributes:['nombreTratamiento']
+        }],
+    });
+    
+    res.json(animal);
+});
+
+
+//consultar todos los animales en tratamiento
+router.get('/enTratamiento', async(req, res) => {
+    const animal = await Animal.findAll(
+        {
+        where: {estadoAnimal: 'en tratamiento'},
+        include:[{
+            model: TipoAnimal,
+            attributes: ['nombreTipoAnimal']
+        },
+        {
+            model: Enfermedad,
+            attributes:['nombreEnfermedad']
+        },
+        {
+            model: Tratamiento,
+            attributes:['nombreTratamiento']
+        }],
+    });
+     res.json(animal);
+});
+
+//consultar todos los animales en proceso de adopcion
+router.get('/enProcesoAdopcion', async(req, res) => {
+    const animal = await Animal.findAll(
+        {
+        where: {estadoAnimal: 'en proceso de adopción'},
+        include:[{
+            model: TipoAnimal,
+            attributes: ['nombreTipoAnimal']
+        },
+        {
+            model: Enfermedad,
+            attributes:['nombreEnfermedad']
+        },
+        {
+            model: Tratamiento,
+            attributes:['nombreTratamiento']
+        },
+        ],
+        
+    });
      res.json(animal);
 });
 
 // CREATE 
-router.post('/', async (req, res) => {
-   req.body.pass = bcrypt.hashSync(req.body.pass, 10);
-   
+router.post('/nuevo', upload.single('fotografia') , async (req, res) => {
+   console.log(req.file)
    const animal = await Animal.create({
        nombreAnimal: req.body.nombreAnimal,
        edad: req.body.edad,
        fechaLlegada: req.body.fechaLlegada,
-       historialTratamiento: req.body.historialTratamiento,
-       idEstadoAnimal_FK: req.body.idEstadoAnimal_FK,
-       idEspecie_FK: req.body.idEspecie_FK
+       motivoLlegada: req.body.motivoLlegada,
+       genero: req.body.genero,
+       estadoAnimal: "en tratamiento",
+       fotografia: req.file,
+       idTipoAnimal_FK: req.body.idTipoAnimal_FK,
+       idEnfermedad_FK: req.body.idEnfermedad_FK,
+       idTratamiento_FK: req.body.idTratamiento_FK,
+
    }).catch(err=>{
-       res.json({err:"error al crear el Animal",detallesError:err.errors[0]});
+       res.json({err:"error al crear el Animal", detallesError:err.errors[0]});
    });
+
    res.status(201).json({success: "Animal Creado Con Exito"});
 });
 
 // UPDATE
-router.put('/actualizar/:idAnimal', async(req, res) => {
+router.put('/actualizar/:idAnimal',upload.single('fotografia'), async(req, res) => {
     const animal = await Animal.update({
-       nombreAnimal: req.body.nombreAnimal,
-       edad: req.body.edad,
-       fechaLlegada: req.body.fechaLlegada,
-       historialTratamiento: req.body.historialTratamiento,
-       idEspecie_FK: req.body.idEspecie_FK
+        nombreAnimal: req.body.nombreAnimal,
+        edad: req.body.edad,
+        fechaLlegada: req.body.fechaLlegada,
+        motivoLlegada: req.body.motivoLlegada,
+        genero: req.body.genero,
+        estadoAnimal: req.body.estadoAnimal,
+        fotografia: req.file,
+        idTipoAnimal_FK: req.body.idTipoAnimal_FK,
+        idEnfermedad_FK: req.body.idEnfermedad_FK,
+        idTratamiento_FK: req.body.idTratamiento_FK,
     },{
         where: { idAnimal: req.params.idAnimal }
     });
      res.json({success:"Animal Actualizado con exito"});
 });
 
-// Cambiar Estado Animal
-router.put('/cambiarEstado/:idAnimal', async(req, res) => {
+// Cambiar estado a adoptado
+router.put('/cambiarEstadoAdoptado/:idAnimal', async(req, res) => {
     const animal = await Animal.update({
-       idEstadoAnimal_FK: req.body.idEstadoAnimal_FK,
-    },{
+        estadoAnimal : 'adoptado'
+    }, {
         where: { idAnimal: req.params.idAnimal }
     });
-     res.json({success:"Estado del Animal Actualizado con exito"});
+    res.status(201).json({message: 'Estado actual adoptado'});
 });
+
+// Cambiar estado a "en tratamiento"
+router.put('/cambiarEstadoEnTratamiento/:idAnimal', async(req, res) => {
+    const animal = await Animal.update({
+        estadoAnimal : 'en tratamiento'
+    }, {
+        where: { idAnimal: req.params.idAnimal }
+    });
+
+    res.status(201).json({message: 'Estado actual en tratamiento'});
+});
+
+
+//Cambiar estado a en proceso de adopcion
+router.put('/cambiarEstadoEnProcesoAdopcion/:idAnimal', async(req, res) => {
+    const animal = await Animal.update({
+        estadoAnimal : 'en proceso de adopción'
+    }, {
+        where: { idAnimal: req.params.idAnimal }
+    });
+
+    res.status(201).json({message: 'Estado actual en proceso de adopción'});
+});
+
 
 module.exports = router;
